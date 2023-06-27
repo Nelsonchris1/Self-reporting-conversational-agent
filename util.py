7b32cc047e8f1754ad1863e24f3b7f56024e61d6
Ogbeide Nelson <ogbeide331@gmail.com>
2023-06-27 14:41:40 +0100

    included documentation for util

--- util.py
+++ util.py
@@ -6,9 +6,9 @@ load_dotenv(find_dotenv())
 
 openai.api_key = os.getenv("OPEN_API2", "NONE")
 
-Initial_content = " "
+Initial_content = ""
 
-def initial_message():
+def initial_message() -> list:
     messages = [
         {"role": "system",
             "content": f"{Initial_content}"},
@@ -17,13 +17,34 @@ def initial_message():
     ]
     return messages
 
-def get_response(messages, model="gpt-3.5-turbo"):
+def get_response(messages:list, model:str="gpt-3.5-turbo") -> str:
+    """
+    Get the response from the language model for a given conversation.
+
+    Args:
+        messages (list): A list of messages representing the conversation.
+        model (str, optional): The model to use for generating the response. Defaults to "gpt-3.5-turbo".
+
+    Returns:
+        str: The generated response from the language model.
+    """
     print("model: ", model)
     response = openai.ChatCompletion.create(model=model,
                                      messages=messages,
                                      )
     return response['choices'][0]['message']['content']
 
-def update_message(messages, role, content):
+def update_message(messages:list, role:str, content:str) -> list:
+    """
+    Add a new message to the conversation.
+
+    Args:
+        messages (list): A list of messages representing the conversation.
+        role (str): The role of the message (e.g., "user", "assistant").
+        content (str): The content of the message.
+
+    Returns:
+        list: The updated list of messages.
+    """
     messages.append({"role": role, "content": content})
     return messages
